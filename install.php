6cad7c82d6ca395608420d1cd7d60f296eeccf9b
Sean Thomas Burke <hawaiianchimp@gmail.com>
2016-05-18 00:05:31 +0000

    hawaiianchimp changed install.php

--- install.php
+++ install.php
@@ -19,9 +19,8 @@ echo '<br>Checking Install<br>';
 if (DBDATABASE && DBUSERNAME && DBSERVER) {
   $sql = 'SELECT COUNT( DISTINCT table_name) FROM information_schema.columns WHERE table_schema = "'.DBDATABASE.'";';
   $result = $mysqli->query($sql);
-  $tables = $result->fetch_all();
-  echo "<br>table count: ".$result[0]."<br>";
-  if ($result[0] === 0) {
+  echo "<br>table count: ".$result->num_rows."<br>";
+  if ($result->num_rows === 0) {
     echo '<br>No tables found, creating tables...<br>';
     $sql = explode(';', file_get_contents('inc/setup.sql'));
     echo 'Getting setup.sql<br>';
@@ -36,48 +35,11 @@ if (DBDATABASE && DBUSERNAME && DBSERVER) {
   }
   else{
     $message = 'Databases and tables have already been installed.<br>';
-    $message .= "If you need to reinstall ESCan go to the <a href='admin.php'>Admin page</a> and reinstall it there";
     die($message);
   }
-  echo '<br>Connecting to Database<br>';
-  $mysqli->execute($sql);
-  echo "<br>Eweek Start Week set<br>";
-  $sql = "SELECT * FROM users WHERE access = ". WEBMASTER .";";
-  $mysqli->query($sql);
-  $web_admins = $mysqli->resultToArray();
-  if (!$mysqli->isEmpty()) {
-    $authorized = false;
-    $emails = '';
-    foreach ($web_admins as $webadmin) {
-      $emails .= '<a href="mailto:'.$webadmin['email'].'">'.$webadmin['email'].'</a>, ';
-      if ($_SESSION['ucinetid'] == $webadmin['ucinetid']) {
-          $authorized = true;
-      }
-    }
-    if(!$authorized)
-    {
-      $sniper = new Sniper();
-      $sniper->storeMessage("Illegall access of install.php", $_SESSION['ucinetid'], "hacker");
-      die('ESCan has already been installed. If you are the webadmin and would like to reinstall ESCan go to the
-      <a href="admin.php">Admin Page</a>. This incident will be reported. Please contact the Web Admin at
-      '.$emails.' or <a href="mailto:'.ORG_EMAIL.'">'.ORG_EMAIL.'</a> if you feel you received this message in error');
-    }
-  } else {
-      echo 'Creating Webmaster<br>';
-      echo 'Inserting Webmaster<br>';
-      $sql = 'REPLACE INTO `users` VALUES("'.WEBMASTER_USERNAME.'", "", "", "'.WEBMASTER_EMAIL.'", "", "", 1, 8, 1, "", "", "_setup.php")';
-      $mysqli->execute($sql);
-      $sql = 'REPLACE INTO `logon` VALUES("'.WEBMASTER_USERNAME.'", "'.md5(WEBMASTER_PASSWORD).'", "", "", "")';
-      $mysqli->execute($sql);
-      echo 'Insertion complete<br>';
-      echo 'Done<Br>';
-      $mysqli->close();
-      echo 'Disconnecting from Database<Br>';
-      echo '<a href="admin.php#intro">Click here to login and setup ESCan</a>';
-    }
-  } else {
-  	$message = 'Please enter in all credentials on the _config.php found in "/inc/setup/_config.php".<br>';
+} else {
+  	$message = 'Please enter in all credentials on the /inc/passwords.php<br>';
   	$message .= "<br>WEBMASTER_USERNAME: ".WEBMASTER_USERNAME."<br>WEBMASTER_EMAIL: ".WEBMASTER_EMAIL."<br>WEBMASTER_PASSWORD: ".WEBMASTER_PASSWORD ."<br>DBDATABASE: ". DBDATABASE ."<br>DBUSERNAME: ". DBUSERNAME ."<br>DBSERVER: ". DBSERVER ."<br>DBPASSWORD: ".DBPASSWORD."<br>";
   	die($message);
-  }
+}
 ?>
