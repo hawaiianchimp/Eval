2cde1be8aa989aa2b53d036123cecca558a3b28f
Sean Thomas Burke <hawaiianchimp@gmail.com>
2016-05-24 22:20:16 +0000

    fixing non refresh on new user or delete user

--- components/data-entry-delete.php
+++ components/data-entry-delete.php
@@ -30,7 +30,7 @@
       $form: $form,
       url: 'api/delete.php',
       submitOnBlur: false,
-      enableFormRefreshOnSubmit: enableFormRefreshOnSubmit,
+      enableFormRefreshOnSubmit: true,
       enableFirstFocus: false,
       successText: 'Deleted!'
     }
